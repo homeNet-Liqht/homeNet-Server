56796b376fe1406287cb8a2356262d49f41446df
thangtran238 <bondshino238@gmail.com>
2024-02-21 16:59:50 +0700

    test deploy

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,9 @@ const authRoute = require('./authRoute')
 
 const route = (app) => {
     app.use('/auth', authRoute);
+    app.use('/', async (req, res) => {
+        res.send("Hello World")
+    });
 }
 
 module.exports = route
