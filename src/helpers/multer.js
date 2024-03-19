8a515f424be8b867f3977f7a38f253bdc186ff83
thangtran238 <bondshino238@gmail.com>
2024-03-19 08:32:23 +0700

    fix: multer litmit update

--- src/helpers/multer.js
+++ src/helpers/multer.js
@@ -3,6 +3,10 @@ const multer = require("multer");
 const storage = multer.memoryStorage();
 const upload = multer({
   storage: storage,
+  limits: {
+    fieldNameSize: 150, 
+    fieldSize: 1024 * 1024 * 10 * 2, 
+  },
 });
 
 module.exports = upload;
