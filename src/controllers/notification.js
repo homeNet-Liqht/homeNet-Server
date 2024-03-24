37a11938496bb2a200725077637a31625653cf28
thangtran238 <bondshino238@gmail.com>
2024-03-24 19:38:54 +0700

    fix

--- src/controllers/notification.js
+++ src/controllers/notification.js
@@ -21,33 +21,7 @@ const notificationController = {
         }
       }
 
-      if (
-        req.body.type === "task" ||
-        req.body.type === "update" ||
-        req.body.type === "finish" ||
-        req.body.type === "delete"
-      ) {
-        const isAssigner = await Task.findById(req.body.task_id);
 
-        if (!isAssigner || isAssigner.assigner != req.idDecoded) {
-          return res.status(404).json({
-            code: 404,
-            data: "Sender isn't the assigner of this task",
-          });
-        }
-
-        const isInTask = await Task.findOne({
-          _id: req.body.task_id,
-          assignees: { $in: receivers },
-        });
-
-        if (!isInTask) {
-          return res.status(404).json({
-            code: 404,
-            data: "There is someone who isn't in this task",
-          });
-        }
-      }
 
       let taskTitle = "";
       if (
